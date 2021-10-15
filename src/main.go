33b8d8473ef617c47a9983d1ea2c8099f1e07ae2
Alan Gilbert <alanhgilbert@gmail.com>
2021-10-15 10:46:24 -0500

    Adding more API stubs

--- src/main.go
+++ src/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -12,12 +12,23 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "welcome\n")
 }
 
-func Upload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {}
+func Upload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "ack")
+}
+func ListImages(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "I know about several images")
+}
+func ShowImage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "here is image %s\n", ps.ByName("imageId"))
+}
 
 func main() {
 
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.POST("/images/", Upload)
+	router.GET("/images/", ListImages)
+	router.GET("/images/:imageId", ShowImage)
 
 	log.Println("Starting Server on 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
