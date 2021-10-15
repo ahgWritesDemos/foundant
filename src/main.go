5c1efb5e71b822df69fd6b214d71226a4c24c5b0
Alan Gilbert <alanhgilbert@gmail.com>
2021-10-15 11:37:55 -0500

    start working with the filesystem

--- src/main.go
+++ src/main.go
@@ -2,24 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type Image struct {
+	Filename string
+	Body     []byte
+}
+
+func (i *Image) save() error {
+	filename := "uploads/" + i.Filename // TODO proper path lib?
+	return ioutil.WriteFile(filename, i.Body, 0600)
+}
+
+func loadImage(filename string) (*Image, error) {
+	body, err := ioutil.ReadFile("uploads/" + filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Image{Filename: filename, Body: body}, nil
+}
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "welcome\n")
+	fmt.Fprint(w, "welcome to the image server\n")
 }
 
 func Upload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "ack")
+	fmt.Fprintf(w, "I have uploaded your image")
 }
+
 func ListImages(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "I know about several images")
 }
+
 func ShowImage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "here is image %s\n", ps.ByName("imageId"))
+	filename := ps.ByName("imageId")
+	img, err := loadImage(filename)
+	if err != nil {
+		fmt.Fprintf(w, "error reading file: %s", err)
+		return
+	}
+	fmt.Fprintf(w, string(img.Body))
 }
 
 func main() {
